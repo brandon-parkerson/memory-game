3cdf145399b4e27fc5a386bfd3f9f1f3d8dc970d
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-02-02 01:17:50 +0200

    api request 12 gifs

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+
 export default function Cards() {
+    const [giphs, setGiphs] = useState([]);
+    useEffect(() => {
+        const url = "https://api.giphy.com/v1/gifs/trending?api_key=hLSP2oSz2V1yhrEImK3W85XXG95Lxx46&limit=12&offset=0&rating=g&bundle=messaging_non_clips";
+        fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            setGiphs(data.data)
+            console.log(data.data);
+        });
+    }, [])
+    
     return (
-        <div className="cards-container"></div>
-    )
-}
+        <div className="cards-container">
+             
+         {giphs.map((giph) => (
+            <img key={giph.id} src={giph.images.fixed_height.url} alt={giph.title} className="giph" />
+        ))} 
+      
+        </div>
+    );
+};
