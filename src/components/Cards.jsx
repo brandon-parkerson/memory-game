51d5a49843d349df8bca69d308dc9060c4662a29
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-02-02 12:33:16 +0200

    feat: shuffle gifs on refresh

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from "react";
 
 export default function Cards() {
+    function shuffle(array) {
+        let currentIndex = array.length;
+        while (currentIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+    }
     const [giphs, setGiphs] = useState([]);
     useEffect(() => {
-        const url = "https://api.giphy.com/v1/gifs/trending?api_key=hLSP2oSz2V1yhrEImK3W85XXG95Lxx46&limit=12&offset=0&rating=g&bundle=messaging_non_clips";
+        const url = "https://api.giphy.com/v1/gifs/trending?api_key=O7b1WBGxC5yDTbKcPKkLdJNROEzuitYy&limit=12&offset=0&rating=g&bundle=messaging_non_clips";
         fetch(url)
         .then((res) => {
             return res.json();
@@ -11,8 +20,10 @@ export default function Cards() {
         .then((data) => {
             setGiphs(data.data)
             console.log(data.data);
+            
         });
     }, [])
+    shuffle(giphs);
     
     return (
         <div className="cards-container">
